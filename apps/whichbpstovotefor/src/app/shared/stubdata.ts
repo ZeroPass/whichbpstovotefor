cc361c1c13b0899c78caae6d91e419927f2487fd
Sarah Yurchi <4070571+syurchi@users.noreply.github.com>
2019-03-25 22:14:16 -0400

    Fixed styles and position of back/progress; stubdata question has more info for testing question component

--- apps/whichbpstovotefor/src/app/shared/stubdata.ts
+++ apps/whichbpstovotefor/src/app/shared/stubdata.ts
@@ -83,7 +83,7 @@ export const PROPOSALS: Proposal[] = [
     {
         name: "prop1",
         title: "Some cool EOS question?",
-        content: "Some accurate supporting description of the question to help you understand what we are asking."
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,<br><br> remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br><br> [https://google.com](https://google.com)"
     }, {
         name: "prop2",
         title: "Some cool EOS question 2?",

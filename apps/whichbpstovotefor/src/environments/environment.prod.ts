b2cded7e52b4ce914209808d762dafdb29d9d082
mili <matthewmilitante@gmail.com>
2019-04-23 23:25:47 -0400

    added production enviroment files

---
+++ apps/whichbpstovotefor/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  URL: 'https://whichbpstovotefor.com'
+};
+
+// ** EOS Mainnet **
+export const network = {
+  blockchain: 'eos',
+  protocol: 'https',
+  host: 'nodes.get-scatter.com',
+  port: 443,
+  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+};
+
+export const contractName = 'eosio.forum';
+
